f29352023a9eb87dacbb1ba968f9dd52c4b3e1c8
sharath <returncode13>
2018-11-23 13:16:00 +0530

    Changed paths for dataPrep stage

--- segyread.sh
+++ segyread.sh
@@ -1,1 +1,1 @@
-sfsegyread < MC1501-1324P1084.sgy tfile=MC1501-1324P1084.tfile hfile=MC1501-1324P1084.hfile datapath=../tmp endian=y verb=y > ../rsf/MC1501-1324P1084.rsf
+sfsegyread < MC1501-1324P1084.sgy tfile=MC1501-1324P1084.tfile hfile=MC1501-1324P1084.hfile datapath=USE_FULL_PATH_NAME endian=1 verb=y > ../rsf/MC1501-1324P1084.rsf
