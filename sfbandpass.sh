861d5dcbe5fb3e11c2cf164723d5c002b10011f9
sharath <returncode13>
2018-11-24 16:50:37 +0530

    for anduril

--- sfbandpass.sh
+++ sfbandpass.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-inputdir="/media/ebs_3tb/poc-machine-seis/data/biased"
-outputdir="/media/ebs_3tb/poc-machine-seis/data/filtered"
+inputdir="/home/ubuntu/programming/data/rsf/field_data_to_test_cnn/biased"
+outputdir="/home/ubuntu/programming/data/rsf/field_data_to_test_cnn/filtered"
 array=($(ls $inputdir/*))
 for f in `ls $inputdir`
 do
 
   output="filtered_"$f
-  sfbandpass < $inputdir/$f > $outputdir/$output flo=1 fhi=125 verb=y
+  sfbandpass < $inputdir/$f > $outputdir/$output flo=1 fhi=62.5 verb=y
   echo "bandpass on "$f
 done
--- shotExtractor.sh
+++ shotExtractor.sh
@@ -2,15 +2,15 @@
 
 traces=648
 tracesToSkip=0
-inputData="/media/ebs_3tb/poc-machine-seis/rsf/MC1501-1396P1078.rsf"
-outputdir="/media/ebs_3tb/poc-machine-seis/data/biased"
+inputData="/home/ubuntu/programming/data/rsf/MC1501-1396P1078.rsf"
+outputdir="/home/ubuntu/programming/data/rsf/field_data_to_test_cnn/biased"
 
 	for ((n=1; n<100 ;n++))
 	do
 	echo "Traces to skip "$tracesToSkip" channels= "$traces" n= "$n
 	output="shot_"$n".rsf"
 	#sfwindow < $inputData   f2=$tracesToSkip n2=$traces o1=0 o2=1 d1=0.004 d2=1 >$outputdir/$output
-	sfwindow < $inputData   f2=$tracesToSkip n2=$traces  >$outputdir/$output
+	sfwindow < $inputData   f2=$tracesToSkip n2=$traces j1=4  >$outputdir/$output
 	tracesToSkip=$tracesToSkip+$traces
 	#n=$((n+1))
 	done
